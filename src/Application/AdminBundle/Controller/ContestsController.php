e55d1fbbd43aab82c3b7ae1a0a66db726159ca5d
Yury Istomenok <iyl@tut.by>
2015-06-11 18:26:00 +0300

    add admin templates

---
+++ src/Application/AdminBundle/Controller/ContestsController.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Club Hg-Product
+ *
+ * Contests controller
+ *
+ * @author     Yury Istomenok <iyl@tut.by>
+ * @copyright  2015 IYL
+ */
+namespace Application\AdminBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+
+/**
+ * Application\AdminBundle\Controller\ContestsController
+ */
+class ContestsController extends Controller
+{
+
+    /**
+     * @Template()
+     *
+     * @return array
+     */
+    public function indexAction()
+    {
+        return array();
+    }
+
+}
